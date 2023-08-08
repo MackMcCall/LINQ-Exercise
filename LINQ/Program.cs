c75b3c2ffd0404d677f73dd5e5a82214f2b1066c
Mack McCall <mackmc9924@gmail.com>
2023-08-08 13:10:46 -0600

    completed exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videogames = new List<string>() 
+            { 
+                "Super Smash Bros Ultimate", 
+                "DOOM", 
+                "Mario Party", 
+                "Splatoon 3", 
+                "EDF 4.1"
+            };
+            var sortedGames = videogames.OrderBy(x => x.Length);
+            foreach (string game in sortedGames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
